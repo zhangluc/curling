2ba1d630474a01693194a10da2ffc36754a61a76
Brentkong <107272510+Brentkong@users.noreply.github.com>
2026-01-01 15:32:00 -0800

    graphs

--- figures/analysis.py
+++ figures/analysis.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 import json
+
 
 with open('/Users/brentkong/Documents/curling/figures/simulations/frequency_dict_10000.json', 'r') as f:
     data = json.load(f)
@@ -8,7 +9,32 @@ frequency_dict = data[0]
 wins_dict = data[1]
 loss_dict = data[2]
 draws_dict = data[3]
+frequency_dict.pop("matches")
+pp_matches = sum(frequency_dict.values())
 
-matches = frequency_dict.pop("matches")
+hammer_start = data[4]["hammer_start"]
+hammer_no_start = data[4]["no_hammer_start"]
+hammer_start_matches = hammer_start.pop("matches")
+hammer_no_start_matches = hammer_no_start.pop("matches")
 
-frequency = True
+frequency_percent_dict, win_percent_dict, draw_percent_dict, loss_percent_dict, win_draw_dict = ({} for _ in range(5))
+for key, value in frequency_dict.items():
+    frequency_percent_dict[key] = frequency_dict[key] / pp_matches
+    win_percent_dict[key] = (wins_dict[key]) / (frequency_dict[key] if frequency_dict[key] != 0 else 1)
+    draw_percent_dict[key] = (draws_dict[key]) / (frequency_dict[key] if frequency_dict[key] != 0 else 1)
+    loss_percent_dict[key] = (loss_dict[key]) / (frequency_dict[key] if frequency_dict[key] != 0 else 1)
+    win_draw_dict[key] = (wins_dict[key] + draws_dict[key]) / (frequency_dict[key] if frequency_dict[key] != 0 else 1)
+
+
+df = pd.DataFrame({
+    'End': [int(k) for k in frequency_dict.keys()],
+    'Frequency %': list(frequency_percent_dict.values()),
+    'Win %': list(win_percent_dict.values()),
+    'Draw %': list(draw_percent_dict.values()),
+    'Loss %': list(loss_percent_dict.values()),
+    'Win + Draw %': list(win_draw_dict.values())
+})
+
+df.index = df.index.astype(int)
+
+df.to_csv('/Users/brentkong/Documents/curling/figures/analysis/analysis.csv', index = False)
--- figures/graphs.py
+++ figures/graphs.py
@@ -8,16 +8,11 @@ frequency_dict = data[0]
 wins_dict = data[1]
 loss_dict = data[2]
 draws_dict = data[3]
+hammer_analysis = data[4]
 matches = frequency_dict.pop("matches")
+mode = "win-draw"
 
-hammer_start = data[4][0]
-hammer_no_start = data[4][1]
-hammer_start_matches = data[4][0].pop("matches")
-hammer_no_start_matches = data[4][1].pop("matches")
-
-frequency = True
-
-if frequency: 
+if mode == "frequency": 
     categories = list(frequency_dict.keys())
     frequencies = list(frequency_dict.values())
 
@@ -28,7 +23,7 @@ if frequency:
     plt.ylabel('Frequency')
     plt.savefig('/Users/brentkong/Documents/curling/figures/graphs/Frequency_End.png')
     plt.show()
-else:
+elif mode == "win-draw": 
     win_draw_dict = {}
     for key, value in frequency_dict.items():
         win_draw_dict[key] = (wins_dict[key] + draws_dict[key]) / (frequency_dict[key] if frequency_dict[key] != 0 else 1)
@@ -37,9 +32,32 @@ else:
     frequencies = list(win_draw_dict.values())
 
     plt.figure(figsize=(8, 6))
-    plt.bar(categories, frequencies, color='skyblue')
+    plt.bar(categories, frequencies, color='#8dd3c7')
     plt.title('Win-Draw Percentage per PowerPlay called at a specific end')
     plt.xlabel('End')
     plt.ylabel('Percentage')
     plt.savefig('/Users/brentkong/Documents/curling/figures/graphs/Win_Draw_Per_End.png')
+    plt.show()
+else:
+    hammer_counts = hammer_analysis['hammer_start']
+    no_hammer_counts = hammer_analysis['no_hammer_start']
+
+    labels = ['Wins', 'Draws', 'Losses']
+
+    hammer_values = [hammer_counts['wins'], hammer_counts['draws'], hammer_counts['losses']]
+    no_hammer_values = [no_hammer_counts['wins'], no_hammer_counts['draws'], no_hammer_counts['losses']]
+    colors = ['#8dd3c7', '#ffffb3', '#fb8072']
+
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.pie(hammer_values, labels=labels, autopct='%1.1f%%', startangle=90, colors=colors)
+    plt.title('Root Team Starts with Hammer')
+
+    plt.subplot(1, 2, 2)
+    plt.pie(no_hammer_values, labels=labels, autopct='%1.1f%%', startangle=90, colors=colors)
+    plt.title('Root Team Starts without Hammer')
+
+    plt.tight_layout()
+    plt.savefig('/Users/brentkong/Documents/curling/figures/graphs/hammer_vs_no_hammer_pie.png')
     plt.show()
